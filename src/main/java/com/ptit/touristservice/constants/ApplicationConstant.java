c930a485fa7e87da52e17100833dbd81e0dfdb19
tuananhkidd1996 <tuananhkidd1996@gmail.com>
2018-11-18 19:45:32 +0700

    update

--- src/main/java/com/ptit/touristservice/constants/ApplicationConstant.java
+++ src/main/java/com/ptit/touristservice/constants/ApplicationConstant.java
@@ -1,7 +1,7 @@
 package com.ptit.touristservice.constants;
 
 public class ApplicationConstant {
-    public static final String LOCAL_HOST = "localhost:9000";
+    public static final String LOCAL_HOST = "35.202.46.112:9000";
     public static final String HOST = "35.184.64.101:6789";
     public static final String WEB_HOST = "35.198.239.45:4300";
     public static final String USER_FILE_ROOT_DIRECTORY = "C://UserFile//";
